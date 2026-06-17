d0a96663c9603a63ba0f91fc536d2ff2e42cc076
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-06-17 19:44:54 +0530

    Time: 1 ms (100%), Space: 42.5 MB (89.78%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,19 +1,20 @@
 class Solution {
     public int arrangeCoins(int n) {
-        if(n==1){
-            return 1;
-        }
-        long sum = 0;
-        int res = n;
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-            if (res < sum) {
-                ans = i - 1;
-                break;
-            }
-        }
+        // if(n==1){
+        //     return 1;
+        // }
+        // long sum = 0;
+        // int res = n;
+        // int ans = 0;
+        // for (int i = 1; i <= n; i++) {
+        //     sum += i;
+        //     if (res < sum) {
+        //         ans = i - 1;
+        //         break;
+        //     }
+        // }
 
-        return ans;
+        // return ans;
+        return (int) ((Math.sqrt(8L*n+1)-1)/2);
     }
 }
