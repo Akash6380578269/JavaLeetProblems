ac96b2e34563768ac994a72e2249940a9a87c35e
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-06-24 22:51:44 +0530

    Time: 1 ms (100%), Space: 43.3 MB (81.18%) - LeetHub

---
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countKeyChanges(String s) {
+        String s1 = s.toLowerCase();
+        int count = 0;
+        for (int i = 1; i < s1.length(); i++) {
+            if (s1.charAt(i) != s1.charAt(i - 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
