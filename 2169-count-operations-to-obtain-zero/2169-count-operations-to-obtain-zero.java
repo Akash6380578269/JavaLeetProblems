43e88093441b1d37ce3fd568e91b66be617ddda9
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-07-07 15:23:16 +0530

    Time: 1 ms (84.93%), Space: 42.2 MB (49.92%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countOperations(int num1, int num2) {
         int count = 0;
-        while (num1 != 0 & num2 != 0) {
+        while (num1 != 0 && num2 != 0) {
             int sum = 0;
             if (num1 >= num2) {
                 sum = num1 - num2;
