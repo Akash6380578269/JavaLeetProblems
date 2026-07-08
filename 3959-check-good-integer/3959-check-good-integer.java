907e7673708b0cf172dc52e3ba843b68041b5909
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-07-08 11:31:33 +0530

    Time: 1 ms (95.54%), Space: 42.6 MB (47.39%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.java
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -5,12 +5,9 @@ class Solution {
         while (n > 0) {
             int d = n % 10;
             dsum += d;
-            sqsum += Math.pow(d, 2);
+            sqsum += d * d;
             n /= 10;
         }
-        if (sqsum - dsum >= 50) {
-            return true;
-        }
-        return false;
+        return sqsum - dsum >= 50;
     }
 }
