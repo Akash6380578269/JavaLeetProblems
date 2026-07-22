cb11dec304cffb1483058a2999aedad584ac0ae8
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-07-22 09:49:40 +0530

    Time: 737 ms (16.68%), Space: 42.9 MB (68.7%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,15 +1,20 @@
 class Solution {
 
-    static boolean isPal(String t) {
-        return t.equals(new StringBuilder(t).reverse().toString());
+   static boolean isPal(String s, int left, int right) {
+    while (left < right) {
+        if (s.charAt(left) != s.charAt(right))
+            return false;
+        left++;
+        right--;
     }
-
+    return true;
+}
     public int countSubstrings(String s) {
 
        int count = 0;
        for (int i = 0; i < s.length(); i++) {
             for (int j = i+1;j<=s.length();j++) {
-                   if(isPal(s.substring(i,j))){
+                   if(isPal(s,i,j-1)){
                         count++;
                    }
             }
