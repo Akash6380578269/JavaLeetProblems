6cb74a88eca6f15ed48a205cd55abaa399b48449
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-07-26 19:09:29 +0530

    Time: 13 ms (30.23%), Space: 48 MB (10.77%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+
+        int[] copy = score.clone();
+        Arrays.sort(copy);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int rank = 1;
+
+        for (int i = copy.length - 1; i >= 0; i--) {
+            map.put(copy[i], rank++);
+        }
+
+        String[] ans = new String[score.length];
+
+        for (int i = 0; i < score.length; i++) {
+
+            int r = map.get(score[i]);
+
+            if (r == 1)
+                ans[i] = "Gold Medal";
+            else if (r == 2)
+                ans[i] = "Silver Medal";
+            else if (r == 3)
+                ans[i] = "Bronze Medal";
+            else
+                ans[i] = String.valueOf(r);
+        }
+
+        return ans;
+    }
+}
