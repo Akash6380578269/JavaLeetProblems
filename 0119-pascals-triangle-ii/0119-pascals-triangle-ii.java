0f60022195facae2c13bc3ecf38e9bc45068910f
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-08-02 16:35:55 +0530

    Time: 2 ms (26.99%), Space: 42.7 MB (19.61%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            List<Integer> rows = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    rows.add(1);
+                } else {
+                    int val = triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j);
+                    rows.add(val);
+                }
+            }
+            triangle.add(rows);
+        }
+        return triangle.get(rowIndex);
+    }
+}
