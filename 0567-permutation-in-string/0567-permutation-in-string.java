1db28a5e0efa01e1dc2e914850cdcf3ca45f6af6
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-08-19 16:09:14 +0530

    Time: 71 ms (14.66%), Space: 47 MB (11.2%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        int n = s1.length();
+
+        if (n > s2.length())
+            return false;
+
+        int[] a = new int[26];
+
+        for (char c : s1.toCharArray()) {
+            a[c - 'a']++;
+        }
+
+        for (int i = 0; i <= s2.length() - n; i++) {
+
+            int[] b = new int[26];
+
+            for (int j = i; j < i + n; j++) {
+                b[s2.charAt(j) - 'a']++;
+            }
+
+            if (same(a, b))
+                return true;
+        }
+
+        return false;
+    }
+
+    public boolean same(int[] a, int[] b) {
+
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i])
+                return false;
+        }
+
+        return true;
+    }
+}
