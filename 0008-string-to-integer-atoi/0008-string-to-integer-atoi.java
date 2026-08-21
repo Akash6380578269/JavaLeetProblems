702b58cf19a4f07ce9a412b6b4a63ed99fe18fc3
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-08-21 09:37:57 +0530

    Time: 1 ms (100%), Space: 43.6 MB (88.24%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,32 +1,20 @@
 class Solution {
     public int myAtoi(String s) {
-
-        int i = 0;
+        long num = 0;
         int sign = 1;
-        double num = 0;
-
-        // Skip spaces
+        int i = 0;
         while (i < s.length() && s.charAt(i) == ' ') {
             i++;
         }
-
-        // Sign
         if (i < s.length() && s.charAt(i) == '-') {
             sign = -1;
             i++;
-        }
-        else if (i < s.length() && s.charAt(i) == '+') {
+        } else if (i < s.length() && s.charAt(i) == '+') {
             i++;
         }
-
-        // Number
         while (i < s.length() && Character.isDigit(s.charAt(i))) {
-
             int digit = s.charAt(i) - '0';
-
             num = num * 10 + digit;
-
-            // Check immediately
             if (num * sign > Integer.MAX_VALUE)
                 return Integer.MAX_VALUE;
 
@@ -34,8 +22,10 @@ class Solution {
                 return Integer.MIN_VALUE;
 
             i++;
+
         }
 
-        return (int)(num * sign);
+        return (int) (num * sign);
+
     }
 }
