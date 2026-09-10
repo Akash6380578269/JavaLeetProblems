836ef78d1839b3cc94f80167baac8de5500c271b
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-09-10 10:54:26 +0530

    Time: 2 ms (99.95%), Space: 49 MB (49.96%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,19 +1,26 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-
+        // Edge case: Since array elements are strictly positive, 
+        // if k is 0 or 1, no product can be strictly less than k.
+        if (k <= 1) return 0; 
+        
         int count = 0;
-        // long pr=1;
-        for (int i = 0; i < nums.length; i++) {
-            long pr = 1;
-            for (int j = i; j < nums.length; j++) {
-                pr *= nums[j];
-                if (pr < k) {
-                    count++;
-                } else {
-                    break;
-                }
+        long pr = 1;
+        int left = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            pr *= nums[right];
+            
+            // If product is too big, shrink the window from the left
+            while (pr >= k) {
+                pr /= nums[left];
+                left++;
             }
+            
+            // Add the number of valid subarrays ending at the 'right' pointer
+            count += right - left + 1;
         }
+        
         return count;
     }
 }
