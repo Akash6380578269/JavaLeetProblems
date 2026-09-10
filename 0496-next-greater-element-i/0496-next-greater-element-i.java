21098d78efa9b2f6daa63f00a0693a922c5cba37
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-09-10 14:02:36 +0530

    Time: 3 ms (93.05%), Space: 45 MB (70.21%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -4,21 +4,22 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = nums2.length - 1; i >= 0; i--) {
+
             while (!st.isEmpty() && st.peek() < nums2[i]) {
                 st.pop();
-
             }
             if (st.isEmpty()) {
                 map.put(nums2[i], -1);
             } else {
                 map.put(nums2[i], st.peek());
             }
-
-            st.push(nums2[i]);
+             st.push(nums2[i]);
         }
+        int res[] = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            nums1[i] = map.get(nums1[i]);
+            res[i] = map.get(nums1[i]);
         }
-        return nums1;
+        return res;
+
     }
 }
