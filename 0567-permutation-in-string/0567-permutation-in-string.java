976c92dd840db73b837081b2f43ea1a19aa49f2c
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-09-16 09:46:25 +0530

    Time: 73 ms (13.93%), Space: 46.7 MB (23.19%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    boolean same(int a[], int b[]) {
+    boolean isSame(int a[], int b[]) {
         for (int i = 0; i < 26; i++) {
             if (a[i] != b[i]) {
                 return false;
@@ -17,10 +17,11 @@ class Solution {
         }
         for (int i = 0; i <= s2.length() - n; i++) {
             int b[] = new int[26];
+
             for (int j = i; j < i + n; j++) {
                 b[s2.charAt(j) - 'a']++;
             }
-            if (same(a, b)) {
+            if (isSame(a, b)) {
                 return true;
             }
 
