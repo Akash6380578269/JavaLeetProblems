6c155e37e795cf862a127879255b8d79e2257a4d
Akash Anbuselvan <162310481+Akash6380578269@users.noreply.github.com>
2026-09-23 11:14:52 +0530

    Time: 72 ms (14.33%), Space: 46.8 MB (17.49%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,23 +1,15 @@
 class Solution {
-
-    boolean isSame(int a[], int b[]) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public boolean checkInclusion(String s1, String s2) {
         int n = s1.length();
         int a[] = new int[26];
-        for (char c : s1.toCharArray()) {
-            a[c - 'a']++;
+        for (int i = 0; i < n; i++) {
+            a[s1.charAt(i) - 'a']++;
         }
-        for (int i = 0; i <= s2.length() - n; i++) {
-            int b[] = new int[26];
+        for (int i = 0; i <= s2.length() - n; i++)
 
+        {
+
+            int b[] = new int[26];
             for (int j = i; j < i + n; j++) {
                 b[s2.charAt(j) - 'a']++;
             }
@@ -27,6 +19,13 @@ class Solution {
 
         }
         return false;
+    }
 
+    static boolean isSame(int a[], int b[]) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i])
+                return false;
+        }
+        return true;
     }
 }
